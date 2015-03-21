4b5770b330381bd2fb974917bfb71e1f2d69cc6a
sameekshacherla <sameekshaceline@gmail.com>
2015-03-20 17:19:36 -0700

    Update TCB.h

--- TCB.h
+++ TCB.h
@@ -4,20 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEBUG 1
-
 typedef struct TCB_t {
-	struct TCB_t *next; // Pointer to next element in chain
-	struct TCB_t *prev; // Pointer to previous element in chain
-	ucontext_t context; // Store information for context switching
+	struct TCB_t *next;
+	struct TCB_t *prev; 
+	ucontext_t context; 
 } TCB_t;
 
 void init_TCB(TCB_t *tcb, void *function, void *stackP, int stack_size) {
-	//memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
-	getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
+	//memset(tcb, '\0', sizeof(TCB_t));       
+	getcontext(&tcb->context);              
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
-	makecontext(&tcb->context, function, 0);// context is now cooked
+	makecontext(&tcb->context, function, 0);
 
 	return;
 }
